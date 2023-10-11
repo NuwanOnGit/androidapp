46a22ee4aa18079a569fb2a01921e3bae5f33000
Kumara PMCN <fas192061@kul.wyb.ac.lk>
2023-10-12 01:58:19 +0530

    Home recycle was connected with Firebase back end and populated with data base data

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.annotation.SuppressLint;
@@ -17,6 +19,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     //Creating fab object
      FloatingActionButton fab;
 
+     DatabaseReference reference;
+     RecyclerView recyclerView;
     ActivityMainBinding binding;
     //@SuppressLint({"NonConstantResourceId", "MissingInflatedId"})
     @Override
@@ -35,11 +46,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        recyclerView=findViewById(R.id.recyclerView);
+        //Connecting to firebase
+        reference
+                = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("event")
+                ;
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Event> list=new ArrayList<>();
+        MyAdapter adapter=new MyAdapter(this,list);
+        recyclerView.setAdapter(adapter);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    Event event=dataSnapshot.getValue(Event.class);
+                    list.add(event);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         //Bottom Navigation Bar code starts here
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
         // Set Home selected
         bottomNavigationView.setSelectedItemId(R.id.home);
+
+
 
         //Floating action button code starts here
         fab = findViewById(R.id.fab); // Assuming your FAB has the id 'fab' in your XML layout file
