ec350029675a97b77a89ca32095a0d9bc69390be
Kumara PMCN <fas192061@kul.wyb.ac.lk>
2023-12-01 10:06:29 +0530

    2023-11-01 [ Empty New Screens were added]

---
+++ app/src/main/java/com/example/androidapp/ViewedPublishedEvent.java
@@ -0,0 +1,14 @@
+package com.example.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewedPublishedEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewed_published_event);
+    }
+}
